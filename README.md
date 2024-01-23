c2a5476d1d5ede63a7e99e94c36ee5eed0c6dfe0
Nakul Bansal <nakulbansal727@gmail.com>
2024-01-23 00:51:22 -0800

    final update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Asciify
+# Asciify
 
 This is a program for generating ascii art in the terminal. Not only does this generate ascii art for an image, but it also supports live video from a webcam. This program is written in C++ and uses the OpenCV library for image processing and ncurses for terminal graphics.
 
@@ -8,11 +8,8 @@ Some cool features of this program include:
 - Option for colored ascii art
 - Resolution control through auto resizing based on terminal size
 
-</br>
 
----
-
-### Installation
+## Installation
 
 To run this program you will need to have OpenCV installed. You can install OpenCV by following the instructions [here](https://docs.opencv.org/4.x/d0/d3d/tutorial_general_install.html). You will also need to have ncurses installed. 
 
@@ -28,9 +25,4 @@ To run the program, run the following command in the root directory of the proje
 
 </br>
 
-
-> **Note:** You need to add all the images you want to use to the `images` directory. The program will not work if you do not have any images in this directory. For customization, you can edit the defined variables in the `src/asciify.cpp` file. 
-
-</br>
-
----
+> **Note:** You need to add all the images you want to use to the `images` directory. The program will not work if you do not have any images in this directory. For customization, you can edit the defined variables in the `src/asciify.cpp` file. There are several factors that play into correct rendering of image and video that are terminal specific to your liking. This includes the terminal theme, color support, terminal font, rendering speed etc. Make sure to use a dark theme.
--- src/asciify.cpp
+++ src/asciify.cpp
@@ -4,50 +4,65 @@
 
 #include "asciify.hpp"
 
-#define ASCII_CHARS ".,:;+*?&%S#@"
-#define CHAR_DISTORTION 1.75
-#define V_BORDER 20
+#define ASCII_CHARS ".,:;+*?&%S#@" // different chars produce different results
+#define CHAR_DISTORTION 2          // 1.75 - 2 usually works
+#define V_BORDER 100
 #define H_BORDER 10
 
 // ----------------------------------------------------------------------------
 
 // HELPER FUNCTIONS
 
+void imgtoAscii(WINDOW *win, cv::Mat &image, int disp_height, int disp_width, int start_x, int start_y)
+{
+    // Pre-calculate ASCII indices
+    cv::Mat ascii_indices = image / 25;
+    for (int i = 0; i < disp_height; ++i)
+    {
+        std::string ascii_row;
+        for (int j = 0; j < disp_width; ++j)
+        {
+            int ascii_index = ascii_indices.at<uchar>(i, j);
+            ascii_row += ASCII_CHARS[ascii_index];
+        }
+        attron(COLOR_PAIR(216));
+        mvwprintw(win, start_y + i, start_x, "%s", ascii_row.c_str());
+        attron(COLOR_PAIR(216));
+    }
+}
 
-void imgtoAscii(WINDOW* win,cv::Mat &image, int disp_height, int disp_width, int start_x, int start_y)
+void imgtoColorAscii(WINDOW *win, cv::Mat &image, int disp_height, int disp_width, int start_x, int start_y)
 {
     for (int i = 0; i < disp_height; ++i)
     {
         for (int j = 0; j < disp_width; ++j)
         {
-            int intensity = image.at<uchar>(i, j);
-            int index = intensity / 25;
-            mvwprintw(win, start_y + i, start_x + j, "%s", ASCII_CHARS[index]);
-        }
-    }
-    return;
-}
-
-void imgtoColorAscii(WINDOW* win, cv::Mat &image, int disp_height, int disp_width, int start_x, int start_y)
-{
-    for (int i = 0; i < disp_height; ++i) 
-    {
-        for (int j = 0; j < disp_width; ++j)
-        {
             cv::Vec3b color = image.at<cv::Vec3b>(i, j);
+
+            // convert the color to 216 color palette
             int r = color[2] / 51;
             int g = color[1] / 51;
             int b = color[0] / 51;
-            int color_pair_index = 36 * r + 6 * g + b + 1; // +1 because color pair 0 is reserved
+            int color_pair_index = (36 * r + 6 * g + b) + 1;
 
+            // find the character corresponding to the intensity
             int intensity = (color[0] + color[1] + color[2]) / 3;
-            int index = intensity / 25;
+            int ascii_index = intensity / 25;
 
-            wattron(win, COLOR_PAIR(color_pair_index));
-            mvwprintw(win, start_y + i, start_x + j, "%c", ASCII_CHARS[index]);
-            wattroff(win, COLOR_PAIR(color_pair_index));
+            // print the character in the corresponding color
+            attron(COLOR_PAIR(color_pair_index));
+            mvwprintw(win, start_y + i, start_x + j, "%c", ASCII_CHARS[ascii_index]);
+            attroff(COLOR_PAIR(color_pair_index));
         }
     }
+}
+
+void drawBorder(WINDOW *win)
+{
+    attron(COLOR_PAIR(216));
+    box(win, 0, 0);
+    attroff(COLOR_PAIR(216));
+    wrefresh(win);
 }
 
 // ----------------------------------------------------------------------------
@@ -78,7 +93,7 @@ WINDOW *initNcurses()
             for (int b = 0; b < 6; ++b)
             {
                 // Create a color and initialize a color pair for it
-                int color_index = (36 * r) + (6 * g) + (b) + (1); // +1 because color 0 is reserved
+                int color_index = (36 * r) + (6 * g) + (b) + (1);
                 init_color(color_index, r * 1000 / 5, g * 1000 / 5, b * 1000 / 5);
                 init_pair(color_index, color_index, COLOR_BLACK);
             }
@@ -86,17 +101,16 @@ WINDOW *initNcurses()
     }
     wbkgd(stdscr, COLOR_BLACK);
 
-    // draw the border
-    box(stdscr, 0, 0);
-    wrefresh(stdscr);
-    getch();
-
-    // display the welcome message on the std screen
+    // draw the border and display the welcome message on the std screen
+    drawBorder(stdscr);
+    attron(COLOR_PAIR(216));
     std::string msg1 = "Welcome to Asciify!";
     std::string msg2 = "Press any key to continue...";
     mvwprintw(stdscr, (LINES / 2), (COLS - msg1.length()) / 2, msg1.c_str());
     mvwprintw(stdscr, (LINES / 2) + 1, (COLS - msg2.length()) / 2, msg2.c_str());
+    attroff(COLOR_PAIR(216));
     wrefresh(stdscr);
+
     getch();
 
     return stdscr;
@@ -156,12 +170,12 @@ cv::Mat readImage(std::string img_name, int color_choice)
 void renderImage(WINDOW *win, cv::Mat &image, int color_choice)
 {
     wclear(win);
-    box(win, 0, 0);
 
     // get the dimension of the current screen
     int t_height, t_width;
     getmaxyx(win, t_height, t_width);
-    if (t_height <= 0 || t_width <= 0) {
+    if (t_height <= 0 || t_width <= 0)
+    {
         std::cerr << "Invalid terminal size: " << t_height << "x" << t_width << std::endl;
         return;
     }
@@ -177,16 +191,17 @@ void renderImage(WINDOW *win, cv::Mat &image, int color_choice)
     aspect_ratio = disp_height / disp_width;
 
     // size it down to fit the terminal height and width
-    if (disp_width > t_width) {
+    if (disp_width > t_width)
+    {
         disp_width = t_width - H_BORDER;
         disp_height = disp_width * aspect_ratio;
-    } 
+    }
     else if (disp_height > t_height)
     {
         disp_height = t_height - V_BORDER;
         disp_width = disp_height / aspect_ratio;
-    } 
-    else 
+    }
+    else
     {
         disp_height = img_height - V_BORDER;
         disp_width = img_width - H_BORDER;
@@ -226,7 +241,8 @@ void renderWebcam(WINDOW *win, int color_choice, int fps)
     while (true)
     {
         // Read the next frame
-        if (!cap.read(frame)) {
+        if (!cap.read(frame))
+        {
             break;
         }
 
